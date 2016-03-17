1fdce1c27e90bac81223ee47c100f3c03668fee1
leonardoH3 <2107262@linux92.labinfo.is.escuelaing.edu.co>
2016-03-17 15:17:55 -0500

    Modificacion ServiciosPacientesDao

--- src/main/java/edu/eci/pdsw/samples/services/ServiciosPacientesDao.java
+++ src/main/java/edu/eci/pdsw/samples/services/ServiciosPacientesDao.java
@@ -7,16 +7,33 @@ package edu.eci.pdsw.samples.services;
 
 import edu.eci.pdsw.samples.entities.Consulta;
 import edu.eci.pdsw.samples.entities.Paciente;
+import edu.eci.pdsw.samples.persistence.DaoFactory;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author 2107262
  */
 public class ServiciosPacientesDao extends ServiciosPacientes{
-    private ArrayList<Paciente> pacientes;
-    //DaoFactory dao = ;
-    @Override
+     DaoFactory daof =null;
+
+    public ServiciosPacientesDao(){
+         try {
+             InputStream input = null;
+             input = getClass().getClassLoader().getResource("applicationconfig_test.properties").openStream();
+             Properties properties=new Properties();
+             properties.load(input);
+             daof=DaoFactory.getInstance(properties);
+         } catch (IOException ex) {
+             Logger.getLogger(ServiciosPacientesDao.class.getName()).log(Level.SEVERE, null, ex);
+         }
+    
+    }
     public Paciente consultarPaciente(int idPaciente, String tipoid) throws ExcepcionServiciosPacientes {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
